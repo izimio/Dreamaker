2d551b1bef7134ab5f5b349afab73ab76b07a585
Izimio <joshua.brionne@gmail.com>
2024-05-02 13:38:49 +0200

    feat(Tools): Adding new route to retrieve the proxyFactory balance

--- backend/src/routes/tools.ts
+++ backend/src/routes/tools.ts
@@ -4,7 +4,12 @@ import { TAGS, LIMITS, ALLOWED_EXTENSIONS } from "../utils/constants";
 import {
     BASE_MINING_DREAMAKER_PERCENTAGE,
     BASE_BOOST_DURATION,
+    DREAM_PROXY_FACTORY_ADDRESS,
 } from "../utils/config";
+import { ABIs, provider } from "../utils/EProviders";
+import { ethers } from "ethers";
+import { InternalError } from "../utils/error";
+
 const router: Router = new Router();
 
 router.prefix("/tools");
@@ -30,6 +35,32 @@ router.get("/constants", async (ctx: Context) => {
                 boostDuration: BASE_BOOST_DURATION,
                 baseMiningRewardPercentage: BASE_MINING_DREAMAKER_PERCENTAGE,
             },
+        },
+    };
+    ctx.status = 200;
+});
+
+router.get("/factory-balance", async (ctx: Context) => {
+
+    const proxyFactory = new ethers.Contract(
+        DREAM_PROXY_FACTORY_ADDRESS,
+        ABIs.ProxyFactory,
+        provider
+    );
+
+    let balance = 0n
+
+    try {
+        balance = await proxyFactory.getBalance();
+        console.log(balance.toString());
+    } catch (e: any) {
+        throw new InternalError("Failed to get factory balance: " + e.message);
+    }
+
+    ctx.body = {
+        ok: true,
+        data: {
+            balance: balance.toString(),
         },
     };
     ctx.status = 200;
